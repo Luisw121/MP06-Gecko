56d5bdcf3042a23ddf0650853da912e4c03825ab
Luisw121 <lafernandez@elpuig.xeill.net>
2023-11-12 19:33:13 +0100

    Cambios

--- src/main/java/net/xeill/elpuig/Main.java
+++ src/main/java/net/xeill/elpuig/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) throws InterruptedException {
         try {
             Scrapper scrapper = new Scrapper();
+            int a = 0;
 
             scrapper.sacarStatsArmas();
         }catch (InterruptedException e) {
