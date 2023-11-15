a8bd4721d04a7a48f3f94f663e54aadc006aaae2
Luisw121 <132393458+Luisw121@users.noreply.github.com>
2023-11-15 18:17:46 +0100

    Update Main.java

--- src/main/java/net/xeill/elpuig/Main.java
+++ src/main/java/net/xeill/elpuig/Main.java
@@ -1,15 +1,39 @@
 package net.xeill.elpuig;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) throws InterruptedException {
-        try {
-            Scrapper scrapper = new Scrapper();
-            int a = 0;
+        Scanner entrada = new Scanner(System.in);
+        boolean N = true;
+        while (N){
+            System.out.println("------MENU------");
+            System.out.println("1. Ver todas las armas");
+            System.out.println("2. Ver todas las llaves");
+            System.out.println("3. Ver todas las cajas");
+            System.out.println("4. Ver todas las skins");
+            int V = entrada.nextInt();
+            switch (V) {
+                case 1:
+                    try {
+                    Scrapper scrapper = new Scrapper();
+                    scrapper.sacarStatsArmas();
+                }catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                    break;
+                case 2:
+                    try {
+                        Scrapper scrapper = new Scrapper();
+                        scrapper.sacarllaves();
+                    }catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                        break;
 
-            scrapper.sacarStatsArmas();
-        }catch (InterruptedException e) {
-            e.printStackTrace();
+            }
         }
+
     }
 }
